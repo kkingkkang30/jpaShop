ffdee2180d323d6fd82166da3ea66670f87fb6bc
82202354 <kkingkkang30@naver.com>
2023-10-22 16:22:11 +0900

    session 1 finish commit

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false) //<- 커밋 하고 테스트 끝남
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memB");
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+        // 같은 트랜잭션 내 . 같은 영속성 안에서 같은 id를 사용하면 같다. 영속성 컨텍스트
+        System.out.println("findMember== member: " + (findMember==member));
+
+    }
+}
