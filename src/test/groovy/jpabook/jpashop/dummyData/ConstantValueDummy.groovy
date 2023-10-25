c814fa98043f17da3417bd3bb7b8fb572c93a4f7
unknown <1176974@hae.hmgdaas.global>
2023-10-25 10:08:13 +0900

    feat: item service

--- src/test/groovy/jpabook/jpashop/dummyData/ConstantValueDummy.groovy
+++ src/test/groovy/jpabook/jpashop/dummyData/ConstantValueDummy.groovy
@@ -1,4 +1,6 @@
 package jpabook.jpashop.dummyData
 
 class ConstantValueDummy {
+    static final MEMBER_ID = 1
+    static final MEMBER_NAME = "sujin"
 }
--- src/test/groovy/jpabook/jpashop/dummyData/MemberDummy.groovy
+++ src/test/groovy/jpabook/jpashop/dummyData/MemberDummy.groovy
@@ -1,4 +1,18 @@
 package jpabook.jpashop.dummyData
 
+import jpabook.jpashop.domain.Member
+
+import static jpabook.jpashop.dummyData.ConstantValueDummy.MEMBER_NAME
+
 class MemberDummy {
+
+    static Member FINE_MEMBER(memberId) {
+        def member = new Member()
+
+        member.id = memberId
+        member.name = MEMBER_NAME
+
+        return member
+    }
+
 }
