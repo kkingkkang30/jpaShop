657023226461f0c7d81fc3fb54e3ae90cd12c65b
unknown <1176974@hae.hmgdaas.global>
2023-10-25 15:03:24 +0900

    feat: order search

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,11 +1,18 @@
 package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.codehaus.groovy.util.StringUtil;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 @RequiredArgsConstructor
@@ -21,6 +28,41 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch){}
+    /*public List<Order> findAll(OrderSearch orderSearch){
 
+       return em.createQuery("select o from Order o join o.member m"
+                       + " where o.status = :status" +
+                         "and m.name like :name", Order.class)
+               .setParameter("status",orderSearch.getOrderStatus())
+               .setParameter("name",orderSearch.getMemberName())
+               //.setFirstResult(100) //100부터 시작해서 1000개 (페이징)
+               .setMaxResults(1000)
+               .getResultList();
+    }*/
+
+    // 에바
+    public List<Order> findByCritria(OrderSearch orderSearch){
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+
+        Root<Order> o = cq.from(Order.class);
+        Join<Object,Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        if(orderSearch.getOrderStatus() != null){
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+
+        if(StringUtils.hasText(orderSearch.getMemberName())){
+            Predicate name =  cb.like(m.get("name"),"%" + orderSearch.getMemberName()+"%");
+            criteria.add(name);
+        }
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+
+        return query.getResultList();
+    }
 }
