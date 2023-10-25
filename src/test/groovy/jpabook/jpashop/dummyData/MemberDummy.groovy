f8f304d9169e2b05a65ee01aaecb7b3202da7677
unknown <1176974@hae.hmgdaas.global>
2023-10-26 08:10:10 +0900

    fix: groovy spock test

--- src/test/groovy/jpabook/jpashop/dummyData/MemberDummy.groovy
+++ src/test/groovy/jpabook/jpashop/dummyData/MemberDummy.groovy
@@ -2,17 +2,21 @@ package jpabook.jpashop.dummyData
 
 import jpabook.jpashop.domain.Member
 
+import java.util.function.Supplier
+
 import static jpabook.jpashop.dummyData.ConstantValueDummy.MEMBER_NAME
+import static jpabook.jpashop.dummyData.ConstantValueDummy.MEMBER_ID
 
 class MemberDummy {
 
-    static Member FINE_MEMBER(memberId) {
+    static Supplier<Member> FINE_MEMBER ={
         def member = new Member()
 
-        member.id = memberId
+        member.id = MEMBER_ID
         member.name = MEMBER_NAME
 
         return member
     }
+
 
 }
